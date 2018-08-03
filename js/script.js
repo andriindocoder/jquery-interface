c1455f1d10faf5a0fd992ab44a78b64e49967c87
Andri Wicaksono <andriwicaksonost@gmail.com>
2018-08-03 14:14:25 +0700

    Initial Template

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(function() {
+
+
+  //READ DATA
+  $.ajax({
+    url: 'js/data.json'
+  }).done(function(data) {
+
+    $('#petList').loadTemplate('appointment-list.html', data, {
+      complete: function() {
+        $('.pet-delete').on('click', function() {
+          $(this).parents('.pet-item').hide(300, function() {
+            $(this).remove();  
+          });
+        }); //delete apt
+      } // complete
+    }); //load template
+
+
+  }); //ajax loaded
+
+  //EVENTS
+
+  $('.apt-addheading').on('click',function() {
+    $('.card-body').toggle(1000);
+  }); // click on add appointment
+
+}); // Document is ready
